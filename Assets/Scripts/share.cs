9a6da547ad2c41987b215acb5961ffb5aecb66ed
Derrick Conway <derrick.conway15@gmail.com>
2018-11-13 18:48:53 +0000

    audio update

---
+++ Assets/Scripts/share.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class share : MonoBehaviour {
+
+    string AppID = "192893404955705";
+
+    string Link = "https://google.com";
+
+    string Caption = "Check out my Score: ";
+
+    string Description = "Enjoy the Game Of Maze Runner";
+	public void shareCountOnFacebook()
+    {
+        Application.OpenURL("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&link=" + Link + "&caption=" + Caption + "&description=" + Description);
+    }
+}
